89acff02ec3cb322538f6228b13385bf16229aa7
LUCIFERLAMO <rithikbravo@gmail.com>
2026-05-30 19:30:27 +0530

    simple update

--- network_scanner.py
+++ network_scanner.py
@@ -2,9 +2,10 @@ import scapy.all as sc
 
 #basic function to do an ARP scan of an given ip 
 def scan(ip):
-    arp_scanner = sc.ARP(pdst=ip) # creating an object for the class .arp()
-    sc.ls(arp_scanner) # will list all the parameters of the class .arp()
-    print(arp_scanner.summary())
+    arp_scanner = sc.ARP(pdst=ip) # creating an object for the class .arp() we r creating an arp packet here as it deals with layer 3.
+    ethernet_frame = sc.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_scan_packet = ethernet_frame / arp_scanner # created a packet which goes in the network broadcast address and send the arp request
+    arp_scan_packet.show()
 
     
 
